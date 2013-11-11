16f89073fe38ebe62f86d1daf68f8b78b3ede512
joel <joel@wtx.com.au>
2013-11-11 05:20:32 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Freakfence::Application.config.secret_key_base = 'ae20f35ce80b263f0c250cb5b6a21113ce6a422c0472d9fdbe35e61d25ed24567fbc506678554f4d16622b914912cf4e32d7cc2f595878dab84c0cb56fbb305f'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Freakfence::Application.config.session_store :cookie_store, key: '_freakfence_session'
